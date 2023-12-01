63a80d78fd542342a25ca153f116f62b2a8948cf
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-11-30 19:04:16 -0500

    initial commit

---
+++ YummyPicks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YummyPicks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
