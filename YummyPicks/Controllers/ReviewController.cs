a68041ca7599530c23ee68f9bee9297632abddda
Paboda Senevirathne <pabodasenevirathne@gmail.com>
2023-12-06 09:10:50 -0500

    bug fixed

--- YummyPicks/Controllers/ReviewController.cs
+++ YummyPicks/Controllers/ReviewController.cs
@@ -20,14 +20,6 @@ namespace YummyPicks.Controllers
             _context = context;
         }
 
-        // GET: Review
-        //public async Task<IActionResult> Index()
-        //{
-        //    return _context.Review != null ?
-        //                View(await _context.Review.ToListAsync()) :
-        //                Problem("Entity set 'YummyPicksContext.Review'  is null.");
-        //}
-
         public async Task<IActionResult> Index(string search)
         {
             var reviews = _context.Review.AsQueryable();
@@ -75,16 +67,6 @@ namespace YummyPicks.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,RestaurantName,FoodName,Price,PublishingDate,Image")] Review review)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(review);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(review);
-        //}
 
         public async Task<IActionResult> Create(ReviewViewModel Re)
         {
@@ -157,22 +139,6 @@ namespace YummyPicks.Controllers
             return View(viewModel);
         }
 
-
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null || _context.Review == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var review = await _context.Review.FindAsync(id);
-        //    if (review == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(review);
-        //}
-
         // POST: Review/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -211,37 +177,6 @@ namespace YummyPicks.Controllers
             }
 
         }
-
-
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,RestaurantName,FoodName,Price,PublishingDate,Image")] Review review)
-        //{
-        //    if (id != review.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(review);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!ReviewExists(review.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(review);
-        //}
 
         // GET: Review/Delete/5
         public async Task<IActionResult> Delete(int? id)
